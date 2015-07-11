f701a2ce080d0e0fd5595a5ffb5d7a26e66357da
otolithic <otolithic@users.noreply.github.com>
2015-07-11 19:39:31 +0000

    Fix conflicts remaining in application helper and homepage html files.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,12 @@
 module ApplicationHelper
-<<<<<<< HEAD
+
   #Returns full title on a per-page basis
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-=======
-  
   def full_title(page_title= '')
     base_title= "Ruby on Rails Tutorial Sample App"
->>>>>>> static-pages
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
   end
-<<<<<<< HEAD
-=======
-  
->>>>>>> static-pages
 end
