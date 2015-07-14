6dc085bc2351dbbac24d1e62561621e570b6418e
otolithic <otolithic@users.noreply.github.com>
2015-07-14 23:20:06 +0000

    Modify tests and test helpers to accomodate new user page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,6 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
-    assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
