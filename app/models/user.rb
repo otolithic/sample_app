53bc44d50d300da507b3f25e8afb7053ac4818d6
otolithic <otolithic@users.noreply.github.com>
2015-11-10 06:05:35 +0000

    Add passwords hashed with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   validates(:email, presence: true, length: { maximum: 255}, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false })
+  before_save {self.email = email.downcase}
+  
+  has_secure_password
 end
