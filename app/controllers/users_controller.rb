489489f86e40ca8ee3115863069b9611522d8bca
otolithic <otolithic@users.noreply.github.com>
2015-11-14 06:30:16 +0000

    Add redirect and flash following successful user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #success
+      flash[:success] = "oh-ho now you've done it"
+      redirect_to @user
     else
       render 'new'
     end
