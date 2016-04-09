6f1c9cdd200911a5e512b9b17a792b66b3c75fb9
otolithic <otolithic@users.noreply.github.com>
2016-04-09 04:50:15 +0000

    Solves exercise 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,16 @@ class User < ActiveRecord::Base
   
   has_secure_password
   
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-  
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
   
   def remember
